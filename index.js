3c9e4fbe365ac6d63525650c7d67f7a516d6cdc9
Peter Stevens <peterstevens230@gmail.com>
2025-05-31 17:59:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 var guesses = document.getElementsByClassName("letter");
 var x = 0;
 
-guesses[x].oninput = function nextLetter() {
-    guesses[x+1].focus();
+function nextLetter() {
     x++;
+    guesses[x].focus();
 }
 
 
